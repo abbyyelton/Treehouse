77fc9364de79d5ce8c29ad533a2fb4eb3ad9cadd
Abby Yelton <abbyyelton@gmail.com>
2016-09-10 22:38:27 -0400

    Completed Sign Up Form project

---
+++ jQuery/sign_up_form_complete/js/app.js
@@ -0,0 +1,51 @@
+//Problem: Hints are shown even when form is valid
+//Solution: Hide and show them at appropriate times
+$password = $("#password");
+$confirmPassword = $("#confirm_password");
+
+function validPassword() {
+  return $password.val().length > 8;
+}
+
+function passwordsMatch() {
+  return $confirmPassword.val() === $password.val();
+}
+
+function canSubmit() {
+  return validPassword() && passwordsMatch();
+}
+
+function passwordEvent() {
+  if (validPassword()) {
+    //If password valid, show hint
+    $password.next().hide();
+  } else {
+    //Else, hide hint
+    $password.next().show();
+  }
+}
+
+function confirmPasswordEvent() {
+  if (passwordsMatch()) {
+    //If passwords match, hide hint
+    $confirmPassword.next().hide();
+  } else {
+    //Else, show hint
+    $confirmPassword.next().show();
+  }
+}
+
+function enableSubmitEvent() {
+  $("#submit").prop("disabled", !canSubmit());
+}
+
+//Hide hints
+$("form span").hide();
+
+//Check password length
+$password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+
+//Check to see if passwords match
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+
+enableSubmitEvent();
